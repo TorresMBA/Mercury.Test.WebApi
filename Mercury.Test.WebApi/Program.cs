3dc7aacb26889cffbc7fb2379b8aa1f87fa7e076
TorresMBA <bryan98_tm@hotmail.com>
2026-07-26 17:17:40 -0500

    test deploy

--- Mercury.Test.WebApi/Program.cs
+++ Mercury.Test.WebApi/Program.cs
@@ -2,8 +2,6 @@ using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
 
 builder.Services.AddOpenApi("v1", options => {
